c12e216180249a415e7b309df8bbc41656ecbb1a
unknown <mate.magyari@gmail.com>
2014-01-14 19:45:07 +0000

    stuff

--- src/FirstClojure/core.clj
+++ src/FirstClojure/core.clj
@@ -7,6 +7,8 @@
       (AssertionError. 
         (str "Expected " expected " but was " actual)))))
 
+(defn assert-false [expr] (assert (not expr)))    
+
 ;;conj
 (assert-equals (conj [1 2] 3) [1 2 3])
 
@@ -26,14 +28,17 @@
 (assert-equals (:name john) "John")
 (assert-equals (:name john) (john :name))
 
+;;change map
 (assert-equals (assoc jack :gender "M" :title "Mr")
                {:name "Jack" :age 31 :gender "M" :title "Mr"}) 
+(assert-equals (dissoc jack :age)
+               {:name "Jack"})
 
 ;;sets
 (assert-equals #{:a :b :c} #{:b :c :a} )
 
 
-; elemets functions of sets and vice-versa
+; elements functions of sets and vice-versa
 (def myset #{:a :b :c})
 (assert-equals (:a myset) :a)
 
@@ -42,12 +47,21 @@
                [333 2]) 
 (assert-equals (assoc [1 2] 2 333)
                [1 2 333]) 
-(assert-equals (nth [1 2] 1)
-               2) 
-(assert-equals (get [1 2] 1)
-               2) 
-(assert-equals (get [1 2] 5 6)
-               6) 
+;;create vector
+(assert-equals (vector :a :b) [:a :b]) 
+;;examine vector
+(assert (vector? [1 2])) 
+(assert-false (vector? :a)) 
+(assert-equals (nth [1 2] 1) 2) 
+(assert-equals (get [1 2] 1) 2) 
+(assert-equals (get [1 2] 5 6) 6) 
+(assert-equals (last [1 2]) 2) 
+(assert-equals (peek [1 2]) 2) 
+(assert-equals (first [1 2]) 1)
+;;change a vector
+(assert-equals (pop [1 2 3]) [1 2]) 
+(assert-equals (subvec [1 2 3 4] 1 3) [2 3])
+(assert-equals (replace {:a :aa :c :cc} [:a :b :c]) [:aa :b :cc])
 
 ;;filter
 (defn filter-minors [persons] (filter #(> 18 (:age %)) persons))
