53ad6c6c8cb5237d85728f53367645ea49422858
unknown <mate.magyari@gmail.com>
2014-04-21 21:21:40 +0100

    merge

--- src/blackjack/game.clj
+++ src/blackjack/game.clj
@@ -4,7 +4,7 @@
 (def suites [:club :heart :spade :diamond])
 (def ranks [:2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K :A])
 
-(def new-deck
+(defn new-deck []
   (shuffle 
     (for [suite suites
           rank ranks]
@@ -12,6 +12,20 @@
 
 (defn draw [deck]
   [(first deck) (rest deck)])
+
+(defn new-game [dealer player]
+  {player { :cards #{} }
+   dealer { :cards #{} }
+   :deck (take 5 (new-deck))})
+
+(defn hit [game player]
+  (let [[card deck] (draw (game :deck))
+        g1 (update-in game [player :cards] #(cons card %))
+        g2 (assoc g1 :deck deck)]
+    g2))
+
+(defn stand [game player]
+  (assoc-in game [player :state] :stand))
 
 ;;(defprotocol GameBehaviour
 ;;  (hit [this player])
