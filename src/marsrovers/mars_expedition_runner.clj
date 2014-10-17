05f087f9d33fbd11a22b096c5d2743069fa46434
mate.magyari <mate.magyari@gmail.com>
2014-10-17 15:07:55 +0200

    main

--- src/marsrovers/mars_expedition_runner.clj
+++ src/marsrovers/mars_expedition_runner.clj
@@ -2,33 +2,27 @@
   ^{:author mate.magyari
     :doc "Expedition runner"}
   marsrovers.mars-expedition-runner
-  (:require [clojure.core.async :as a]
-            [marsrovers.app :as app]
+  (:require [marsrovers.app :as app]
             [marsrovers.glue :as glue]
             [marsrovers.expedition-config-reader :as ecr]))
 
-(def displayer-channel (glue/chan))
-(def plateau-channel (glue/chan))
-(def nasa-hq-channel (glue/chan))
+(defn -main []
+  (let [displayer-channel (glue/chan)
+        plateau-channel (glue/chan)
+        nasa-hq-channel (glue/chan)
+        time-stamp (System/currentTimeMillis)
+        expedition-config (ecr/expedition-config)
+        dim-screen [600 600]]
+    (do
+      (println (str (- (System/currentTimeMillis) time-stamp) " ms has elapsed"))
+      (println "Word starting...")
+      (app/start-world! expedition-config plateau-channel nasa-hq-channel displayer-channel dim-screen)
+      (println "Word started")
+      (app/start-rovers!
+        (:rover-configs expedition-config)
+        plateau-channel
+        nasa-hq-channel)
+      (println "Rovers started up"))))
 
-(def time-stamp (System/currentTimeMillis))
-(println "Reading up expedition config...")
-(def expedition-config (ecr/expedition-config))
-(def dim-screen [600 600])
-
-(println (str (- (System/currentTimeMillis) time-stamp) " ms has elapsed"))
-(println "Word starting...")
-(app/start-world! expedition-config plateau-channel nasa-hq-channel displayer-channel dim-screen)
-(println "Word started")
-
-(app/start-rovers!
-  (:rover-configs expedition-config)
-  plateau-channel
-  nasa-hq-channel)
-
-(println "Rovers started up")
-
-(a/<!! (a/timeout 100000))
-(println "End")
-
+(-main)
 
