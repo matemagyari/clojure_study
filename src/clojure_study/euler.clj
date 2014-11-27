edaea12dc99832f711544115a8f4d914433b95c3
mate.magyari <mate.magyari@gmail.com>
2014-11-27 08:55:41 +0000

    refactor

--- src/clojure_study/euler.clj
+++ src/clojure_study/euler.clj
@@ -15,15 +15,15 @@
       :else (recur primes (inc x)))))
 
 (defn power-triples [n]
-  (let [pow-n (fn [exp] (Math/pow n exp)) ; n on the power of exp
+  (let [power-sum (fn [x y z] (+ (Math/pow x 2)
+                                (Math/pow y 3)
+                                (Math/pow z 4)))
+        pow-n (fn [exp] (Math/pow n exp)) ; n on the power of exp
         ps-2 (find-primes-under (pow-n 1/2)) ;primes under square root n
         ps-3 (filter #(<= % (pow-n 1/3)) ps-2) ;primes under cubic root n
         ps-4 (filter #(<= % (pow-n 1/4)) ps-3)] ;primes under forth root n
     (for [p2 ps-2 p3 ps-3 p4 ps-4
-          :let [power-sum (+ (Math/pow p2 2)
-                            (Math/pow p3 3)
-                            (Math/pow p4 4))]
-          :when (<= power-sum n)]
+          :when (<= (power-sum p2 p3 p4) n)]
       [p2 p3 p4])))
 
 (def n 50000000)
